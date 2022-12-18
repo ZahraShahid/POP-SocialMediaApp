a75dbae0bd212c744339a751c4b93b75d3821af9
Zahra Shahid <zashahid45@gmail.com>
2022-12-18 17:55:17 +0500

    Apollo setup to connect  with client

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,19 @@
+import React from "react";
+import App from "./App";
+import {ApolloClient, InMemoryCache, createHttpLink, ApolloProvider} from "@apollo/client";
+// import { ApolloProvider } from "@apollo/react-hooks";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:5550",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+)
